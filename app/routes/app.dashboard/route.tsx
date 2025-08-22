69fddb987920f67afe535e8e77c69f08c65ccf49
ernesto-pm <ernestopm20@gmail.com>
2025-08-22 16:43:30 -0600

    Adding new chart

--- app/routes/app.dashboard/route.tsx
+++ app/routes/app.dashboard/route.tsx
@@ -26,42 +26,109 @@ export default function Dashboard() {
 
                                 <div className="px-4 py-2 bg-sidebar-accent rounded-t-lg flex flex-row">
                                     <div className="flex-1 text-sm font-semibold">
-                                        Total de ventas por mes
+                                        Total de Ordenes por mes
                                     </div>
                                     <div className="flex flex-row gap-2 items-center">
-                                        <Settings className="h-5 text-gray-500 cursor-pointer"/>
+                                        <Settings className="h-4 text-gray-500 cursor-pointer"/>
                                     </div>
                                 </div>
 
                                 <div className="flex-1 p-2">
                                     <LineChart
-                                        horizontalAxisValues={['Jan', 'Feb', 'March', 'June', 'July']}
-                                        horizontalAxisLabel="Mes"
-                                        horizontalAxisType="category"
-                                        verticalAxisValues={[100, 120, 233, 150, 180]}
-                                        verticalAxisLabel="Ventas"
+                                        horizontalAxisValues={[
+                                            "2022-02-01T00:00:00+00:00",
+                                            "2022-04-01T00:00:00+00:00",
+                                            "2022-05-01T00:00:00+00:00",
+                                            "2022-06-01T00:00:00+00:00",
+                                            "2022-07-01T00:00:00+00:00",
+                                            "2022-08-01T00:00:00+00:00",
+                                            "2022-09-01T00:00:00+00:00",
+                                            "2022-10-01T00:00:00+00:00",
+                                            "2022-11-01T00:00:00+00:00",
+                                            "2022-12-01T00:00:00+00:00",
+                                            "2023-01-01T00:00:00+00:00",
+                                            "2023-02-01T00:00:00+00:00",
+                                            "2023-03-01T00:00:00+00:00",
+                                            "2023-04-01T00:00:00+00:00",
+                                            "2023-05-01T00:00:00+00:00",
+                                            "2023-06-01T00:00:00+00:00",
+                                            "2023-07-01T00:00:00+00:00",
+                                            "2023-08-01T00:00:00+00:00",
+                                            "2023-09-01T00:00:00+00:00",
+                                            "2023-10-01T00:00:00+00:00",
+                                            "2023-11-01T00:00:00+00:00",
+                                            "2023-12-01T00:00:00+00:00",
+                                            "2024-01-01T00:00:00+00:00",
+                                            "2024-02-01T00:00:00+00:00",
+                                            "2024-03-01T00:00:00+00:00",
+                                            "2024-04-01T00:00:00+00:00",
+                                            "2024-05-01T00:00:00+00:00",
+                                            "2024-06-01T00:00:00+00:00",
+                                            "2024-07-01T00:00:00+00:00",
+                                            "2024-08-01T00:00:00+00:00",
+                                            "2024-09-01T00:00:00+00:00",
+                                            "2024-10-01T00:00:00+00:00",
+                                            "2024-11-01T00:00:00+00:00",
+                                            "2024-12-01T00:00:00+00:00",
+                                            "2025-01-01T00:00:00+00:00",
+                                            "2025-02-01T00:00:00+00:00",
+                                            "2025-03-01T00:00:00+00:00",
+                                            "2025-04-01T00:00:00+00:00",
+                                            "2025-05-01T00:00:00+00:00",
+                                            "2025-06-01T00:00:00+00:00",
+                                            "2025-07-01T00:00:00+00:00"
+                                        ]}
+                                        horizontalAxisLabel="Fecha"
+                                        horizontalAxisType="time"
+                                        verticalAxisValues={[
+                                            1,
+                                            3,
+                                            47,
+                                            215,
+                                            211,
+                                            212,
+                                            212,
+                                            169,
+                                            170,
+                                            105,
+                                            174,
+                                            165,
+                                            172,
+                                            125,
+                                            167,
+                                            191,
+                                            134,
+                                            158,
+                                            174,
+                                            151,
+                                            145,
+                                            92,
+                                            162,
+                                            173,
+                                            161,
+                                            145,
+                                            180,
+                                            136,
+                                            112,
+                                            174,
+                                            142,
+                                            144,
+                                            143,
+                                            85,
+                                            167,
+                                            173,
+                                            149,
+                                            128,
+                                            178,
+                                            139,
+                                            94
+                                        ]}
+                                        verticalAxisLabel="Total"
                                         verticalAxisType="value"
                                     />
                                 </div>
                             </div>
 
-
-
-                            <div className="col-span-4 row-span-4 rounded-lg bg-sidebar flex flex-col gap-2">
-                                <div className="font-semibold p-3">
-                                    Total de ingresos por mes
-                                </div>
-                                <div className="flex-1 p-2">
-                                    <LineChart
-                                        horizontalAxisValues={['Jan', 'Feb', 'March', 'June', 'July']}
-                                        horizontalAxisLabel="Mes"
-                                        horizontalAxisType="category"
-                                        verticalAxisValues={[140000, 122000, 220000, 70000, 123000]}
-                                        verticalAxisLabel="Ventas"
-                                        verticalAxisType="value"
-                                    />
-                                </div>
-                            </div>
 
                             <div className="p-5 rounded-md bg-sidebar flex flex-col gap-2">
                                 <div className="text-md font-bold text-center">
