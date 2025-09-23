da7fe40cc9aa04415489411fcb9445f0fc5cdae9
ernesto-pm <ernestopm20@gmail.com>
2025-09-23 00:06:48 -0600

    Removing loading text

--- app/components/LoadingOverlay.tsx
+++ app/components/LoadingOverlay.tsx
@@ -11,7 +11,9 @@ export function LoadingOverlay() {
             <div className="flex flex-col items-center space-y-4">
                 {/* Spinner */}
                 <div className="w-12 h-12 border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin"></div>
+                {/*
                 <p className="text-white text-lg font-medium">Cargando, por favor espera un momento...</p>
+                */}
             </div>
         </div>
     );
